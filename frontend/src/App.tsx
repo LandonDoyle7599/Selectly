c8eb00686f8c6235fe6ebba7a0be91600e80c177
medwards <maxedwards20@gmail.com>
2023-03-24 20:40:48 -0600

    Item card with voting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
-    createBrowserRouter,
-    RouterProvider,
-    useLocation,
-} from 'react-router-dom'
-import CreateAccount from './pages/CreateAccount'
-import { ApiContext } from './contexts/api'
-import { Api } from './lib/api'
-import { Login } from './pages/Login'
+  createBrowserRouter,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
+import { ApiContext } from "./contexts/api";
+import { Api } from "./lib/api";
+import CreateAccount from "./pages/CreateAccount";
+import { Demo } from "./pages/Demo";
+import { Login } from "./pages/Login";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        children: [
-            {
-                path: 'login',
-                element: <Login />,
-            },
-            {
-                path: 'create-account',
-                element: <CreateAccount />,
-            },
-        ],
-    },
-])
+  {
+    path: "/",
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "create-account",
+        element: <CreateAccount />,
+      },
+      {
+        path: "demo",
+        element: <Demo />,
+      },
+    ],
+  },
+]);
 
 export const App = () => {
-    const [api, setApi] = useState(new Api())
+  const [api, setApi] = useState(new Api());
 
-    return (
-        <>
-            <ApiContext.Provider value={api}>
-                <RouterProvider router={router} />
-            </ApiContext.Provider>
-        </>
-    )
-}
+  return (
+    <>
+      <ApiContext.Provider value={api}>
+        <RouterProvider router={router} />
+      </ApiContext.Provider>
+    </>
+  );
+};
---
+++ frontend/src/pages/Demo.tsx
@@ -0,0 +1,21 @@
+import { ItemCard } from "../components/ItemCard";
+
+export type VoteType = "like" | "dislike";
+
+export const Demo = () => {
+  const title = "Title";
+  const description = "Description";
+  const imageURL = "https://picsum.photos/200/300";
+  const id = 1;
+  const handleVote = (vote: VoteType) => {};
+
+  return (
+    <ItemCard
+      title={title}
+      description={description}
+      imageURL={imageURL}
+      handleVote={handleVote}
+      id={id}
+    ></ItemCard>
+  );
+};

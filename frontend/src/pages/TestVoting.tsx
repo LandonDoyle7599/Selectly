2eeac6ec752a782b67fd4740f38ae7726a3f1320
LandonDoyle7599 <landondoyle7599@gmail.com>
2023-03-25 15:28:49 -0600

    stuff

--- frontend/src/pages/TestVoting.tsx
+++ frontend/src/pages/TestVoting.tsx
@@ -50,7 +50,7 @@ export const TestVoting: FC<TestVotingProps> = (props) => {
       <Stack sx={{maxHeight: "70%", maxWidth: "20%", height: "100vh", width: "100vw"}}>
       <Card>
         <CardContent sx={{alignItems: "center"}}>
-            <img width="100vw" height="100%" src={activeCard?.photoURL ? activeCard.photoURL : ""} alt={activeCard?.title || ""} />
+            <img width="100vw" height="100%" src={activeCard?.photoURL ? activeCard.photoURL : null} />
           <Typography variant="h4">{activeCard?.title}</Typography>
           <Typography variant="body1" whiteSpace={"pre-line"}>{activeCard?.content}</Typography>
         </CardContent>
